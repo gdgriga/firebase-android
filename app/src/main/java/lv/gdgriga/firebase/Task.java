abbbdd6b18ea048b69ca890a1e4d7bfa844819ad
Valentin-Paramonov <valentine.paramonov@gmail.com>
2016-08-04 00:28:21 +0300

    Add uuid to task

--- app/src/main/java/lv/gdgriga/firebase/Task.java
+++ app/src/main/java/lv/gdgriga/firebase/Task.java
@@ -1,15 +1,20 @@
 package lv.gdgriga.firebase;
 
+import java.util.UUID;
+
 public class Task {
+    public final String id;
     public String title;
     public User assignee;
     public String attachment;
     public Column column;
 
     public Task() {
+        id = UUID.randomUUID().toString();
     }
 
     Task(String title, User assignee, Column column) {
+        this();
         this.title = title;
         this.assignee = assignee;
         this.column = column;
