61834f5d4f11232d3e40b13ef0626e1c81d85501
Valentin-Paramonov <valentine.paramonov@gmail.com>
2016-08-23 23:53:38 +0300

    Prepare foundation for the workshop

--- app/src/main/java/lv/gdgriga/firebase/storage/Storage.java
+++ app/src/main/java/lv/gdgriga/firebase/storage/Storage.java
@@ -19,20 +19,20 @@ public final class Storage {
         // TODO: Upload attachment
     }
 
-    private static /* TODO Replace with StorageReference */ Object storage() {
-        return /*Return FirebaseStorage reference*/ null;
-    }
-
     private static String toGsLink(String bucket, String path) {
         return "gs://" + bucket + "/" + path;
     }
 
-    private static void error(String message) {
-        Log.e(Storage.class.getName(), message);
-    }
-
     public static void getAttachmentStream(String url, Consumer<InputStream> onSuccess) {
         // TODO: Get attachment stream
+    }
+
+    private static int error(String message) {
+        return Log.e(Storage.class.getName(), message);
+    }
+
+    private static /* TODO Replace with StorageReference */ Object storage() {
+        return /*Return FirebaseStorage reference*/ null;
     }
 
     private static Optional</*TODO: Replace with StorageReference */Object> storage(String url) {
