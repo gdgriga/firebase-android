db83c9989087ab1dd34d4587a3578b8b71698a02
Valentin-Paramonov <valentine.paramonov@gmail.com>
2016-08-25 01:03:07 +0300

    Perform sign in

--- app/src/main/java/lv/gdgriga/firebase/user_management/SignInActivity.java
+++ app/src/main/java/lv/gdgriga/firebase/user_management/SignInActivity.java
@@ -5,16 +5,34 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.*;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import lv.gdgriga.firebase.board.BoardActivity;
 
 import static android.widget.Toast.LENGTH_SHORT;
 import static android.widget.Toast.makeText;
+import static com.google.android.gms.auth.api.Auth.GOOGLE_SIGN_IN_API;
+import static com.google.android.gms.auth.api.Auth.GoogleSignInApi;
+import static com.google.android.gms.auth.api.signin.GoogleSignInOptions.DEFAULT_SIGN_IN;
+import static lv.gdgriga.firebase.R.id.sign_in_button;
 import static lv.gdgriga.firebase.R.layout.activity_signin;
+import static lv.gdgriga.firebase.R.string.default_web_client_id;
 
 public class SignInActivity extends AppCompatActivity {
     private static final int signOutCode = 0xCAFE;
     private static final int signInCode = 0xC001;
-    // TODO: Bind sign-in button
+    @BindView(sign_in_button) SignInButton signInButton;
+    private GoogleApiClient googleClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +40,24 @@ public class SignInActivity extends AppCompatActivity {
         setContentView(activity_signin);
         ButterKnife.bind(this);
         getSupportActionBar().hide();
-        // TODO: Set click listener
+        googleClient = new GoogleApiClient.Builder(this).enableAutoManage(this, this::onConnectionFailed)
+                                                        .addApi(GOOGLE_SIGN_IN_API, buildSignInOptions())
+                                                        .build();
+        signInButton.setOnClickListener(this::onSignIn);
+    }
+
+    private GoogleSignInOptions buildSignInOptions() {
+        return new GoogleSignInOptions.Builder(DEFAULT_SIGN_IN).requestIdToken(getString(default_web_client_id))
+                                                               .requestEmail()
+                                                               .build();
+    }
+
+    private void onConnectionFailed(ConnectionResult connectionResult) {
+        toast("Google Play Services error.");
     }
 
     private void onSignIn(View view) {
-        // TODO: Replace with sign in Intent
-        Intent signIn = new Intent();
+        Intent signIn = GoogleSignInApi.getSignInIntent(googleClient);
         startActivityForResult(signIn, signInCode);
     }
 
@@ -43,7 +73,31 @@ public class SignInActivity extends AppCompatActivity {
     }
 
     private void signIn(Intent data) {
-        // TODO: Implement sign in
+        GoogleSignInResult signInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+        if (signInResult.isSuccess()) {
+            signInTo(signInResult.getSignInAccount());
+        } else {
+            toast("Google Sign In failed");
+        }
+    }
+
+    private void signInTo(GoogleSignInAccount account) {
+        FirebaseAuth.getInstance()
+                    .signInWithCredential(fromToken(account.getIdToken()))
+                    .addOnCompleteListener(this, this::onSignInComplete);
+    }
+
+    private void onSignInComplete(Task<AuthResult> signinResult) {
+        if (signinResult.isSuccessful()) {
+            GoogleUser.saveIfNew();
+            startActivityForResult(new Intent(this, BoardActivity.class), signOutCode);
+        } else {
+            toast("Authentication failed.");
+        }
+    }
+
+    private AuthCredential fromToken(String token) {
+        return GoogleAuthProvider.getCredential(token, null);
     }
 
     private void signOut() {
