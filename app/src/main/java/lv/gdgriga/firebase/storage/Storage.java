5e2e85b46de0769e9fd0a04d0b9728a77658d035
Valentin-Paramonov <valentine.paramonov@gmail.com>
2016-08-26 23:46:11 +0300

    Add storage

--- app/src/main/java/lv/gdgriga/firebase/storage/Storage.java
+++ app/src/main/java/lv/gdgriga/firebase/storage/Storage.java
@@ -3,7 +3,12 @@ package lv.gdgriga.firebase.storage;
 import android.net.Uri;
 import android.util.Log;
 
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 
 import java8.util.Optional;
@@ -16,11 +21,16 @@ public final class Storage {
 
     public static void uploadAttachment(String path, Consumer<String> onSuccess) {
         Uri uri = toUri(path);
-        // TODO: Upload attachment
+        storage().child(attachments).child(uri.getLastPathSegment()).putFile(uri)
+                 .addOnSuccessListener(snapshot -> {
+                     StorageMetadata meta = snapshot.getMetadata();
+                     onSuccess.accept(toGsLink(meta.getBucket(), meta.getPath()));
+                 })
+                 .addOnFailureListener(error -> error(error.getMessage()));
     }
 
-    private static /* TODO Replace with StorageReference */ Object storage() {
-        return /*Return FirebaseStorage reference*/ null;
+    private static StorageReference storage() {
+        return FirebaseStorage.getInstance().getReference();
     }
 
     private static String toGsLink(String bucket, String path) {
@@ -32,12 +42,20 @@ public final class Storage {
     }
 
     public static void getAttachmentStream(String url, Consumer<InputStream> onSuccess) {
-        // TODO: Get attachment stream
+        storage(url).ifPresent(ref ->
+            ref.getStream((snapshot, stream) -> {
+                try {
+                    onSuccess.accept(stream);
+                    stream.close();
+                } catch (IOException e) {
+                    error(e.getMessage());
+                }
+            }).addOnFailureListener(error -> error(error.getMessage())));
     }
 
-    private static Optional</*TODO: Replace with StorageReference */Object> storage(String url) {
+    private static Optional<StorageReference> storage(String url) {
         try {
-            return Optional.of(/*TODO: Get reference from URL*/null);
+            return Optional.of(FirebaseStorage.getInstance().getReferenceFromUrl(url));
         } catch (IllegalArgumentException e) {
             return Optional.empty();
         }
@@ -48,6 +66,6 @@ public final class Storage {
     }
 
     public static void deleteAttachment(String url) {
-        // TODO: Delete the attachemnt file
+        storage(url).ifPresent(StorageReference::delete);
     }
 }
